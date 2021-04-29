7619d5e2ea94f89cdd64b8c2f0fb560d0896e48d
Tri Dao <minhtri1396@gmail.com>
2021-04-29 16:40:49 +0700

    chg(test): update test case of HMAC

--- test/utils/hmac_test.dart
+++ test/utils/hmac_test.dart
@@ -148,16 +148,164 @@ void main() {
       76
     ]));
 
-    var data = Uint8List.fromList("Goldeneye Technologies".codeUnits);
-    var isSuccess = await HMAC.validateHMAC(
+    final data = Uint8List.fromList("Goldeneye Technologies".codeUnits);
+    final isSuccess = await HMAC.validateHMAC(
       Uint8List.fromList(key.bytes),
       data,
       Uint8List.fromList(expectedHMAC),
     );
     expect(isSuccess, true);
+  });
+  test('Validate HMAC with wrong data', () async {
+    final expectedHMAC = [
+      47,
+      121,
+      108,
+      13,
+      177,
+      58,
+      58,
+      235,
+      166,
+      113,
+      132,
+      61,
+      39,
+      64,
+      75,
+      230,
+      76,
+      184,
+      174,
+      11,
+      168,
+      45,
+      78,
+      208,
+      199,
+      49,
+      165,
+      159,
+      244,
+      138,
+      139,
+      47,
+    ];
+    final key = await Sha256().hash(Uint8List.fromList([
+      114,
+      203,
+      246,
+      0,
+      37,
+      216,
+      117,
+      58,
+      193,
+      41,
+      160,
+      114,
+      203,
+      246,
+      0,
+      37,
+      216,
+      117,
+      58,
+      193,
+      41,
+      160,
+      124,
+      41,
+      159,
+      100,
+      79,
+      136,
+      82,
+      108,
+      22,
+      76
+    ]));
 
-    data = Uint8List.fromList("Goldeneye Technologies Fake".codeUnits);
-    isSuccess = await HMAC.validateHMAC(
+    final data = Uint8List.fromList("Goldeneye Technologies Fake".codeUnits);
+    final isSuccess = await HMAC.validateHMAC(
+      Uint8List.fromList(key.bytes),
+      data,
+      Uint8List.fromList(expectedHMAC),
+    );
+    expect(isSuccess, false);
+  });
+  test('Validate HMAC with wrong key', () async {
+    final expectedHMAC = [
+      47,
+      121,
+      108,
+      13,
+      177,
+      58,
+      58,
+      235,
+      166,
+      113,
+      132,
+      61,
+      39,
+      64,
+      75,
+      230,
+      76,
+      184,
+      174,
+      11,
+      168,
+      45,
+      78,
+      208,
+      199,
+      49,
+      165,
+      159,
+      244,
+      138,
+      139,
+      47,
+    ];
+    final key = await Sha256().hash(Uint8List.fromList([
+      114,
+      203,
+      246,
+      0,
+      37,
+      216,
+      117,
+      58,
+      193,
+      41,
+      160,
+      114,
+      203,
+      246,
+      0,
+      37,
+      216,
+      117,
+      58,
+      193,
+      41,
+      160,
+      124,
+      41,
+      159,
+      100,
+      79,
+      136,
+      82,
+      109,
+      22,
+      76
+    ]));
+
+    final data = Uint8List.fromList("Goldeneye Technologies".codeUnits);
+    final isSuccess = await HMAC.validateHMAC(
       Uint8List.fromList(key.bytes),
       data,
       Uint8List.fromList(expectedHMAC),
